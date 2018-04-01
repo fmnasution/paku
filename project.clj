cc0e2c068c7be65c21912161eb6d2041c2fe3f91
Faris Nasution <funyako.funyao156@gmail.com>
2018-04-01 10:45:56 +0700

    Bump to 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paku "0.1.5"
+(defproject paku "0.1.6"
   :description "Collection of reusable components"
   :url "https://github.com/fmnasution/paku"
   :license {:name "Eclipse Public License"
